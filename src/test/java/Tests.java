bfa83fcfec756b88f1034df746791f984f2f359c
sausageRoll <drozdovigor1996@gmail.com>
2017-07-24 00:11:52 +0300

    unstatic

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class Tests {
 
     @Test
-    public static void test() {
+    public void test() {
         assertEquals(2.0,Math.sqrt(4.0),0.001);
     }
 
