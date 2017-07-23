90dc7e463eab27cb9a82b5b69a0bb66f8ea14ad3
sausageRoll <drozdovigor1996@gmail.com>
2017-07-23 23:42:20 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * jenkins
+ * Created on 23.07.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Good");
+    }
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * jenkins
+ * Created on 23.07.17.
+ */
+public class Tests {
+
+    @Test
+    public static void test() {
+        assertEquals(2.0,Math.sqrt(4.0),0.001);
+    }
+
+}
